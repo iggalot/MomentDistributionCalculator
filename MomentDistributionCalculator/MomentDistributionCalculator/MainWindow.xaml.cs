f9fc6fb48a6d683f7d5cf0ff49f316801e623f8e
Jim Allen <iggalot.iggy@gmail.com>
2021-03-04 19:13:41 -0600

    Initial commit.
    Draws a line on a canvas object.

---
+++ MomentDistributionCalculator/MomentDistributionCalculator/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MomentDistributionCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Change color of background
+            MainCanvas.Background = new SolidColorBrush(Colors.Cyan);
+            Line myLine = new Line();
+            myLine.Stroke = Brushes.Black;
+            myLine.StrokeThickness = 2;
+            myLine.X1 = 10;
+            myLine.Y1 = 10;
+            myLine.X2 = 400;
+            myLine.Y2 = 400;
+            myLine.HorizontalAlignment = HorizontalAlignment.Left;
+            myLine.VerticalAlignment = VerticalAlignment.Center;
+            MainCanvas.Children.Add(myLine);
+        }
+    }
+}
